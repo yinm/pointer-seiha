b7ade09d044d15ead7711ea21211e2d1a84efeec
yinm <yinm.cminor9@gmail.com>
2018-02-09 13:54:01 +0900

    Check pointer

--- chap1/pointer.c
+++ chap1/pointer.c
@@ -2,12 +2,20 @@
 
 int main(void)
 {
-    printf("_Bool..%d\n", (int)sizeof(_Bool));
-    printf("char..%d\n", (int)sizeof(char));
-    printf("short..%d\n", (int)sizeof(short));
-    printf("int..%d\n", (int)sizeof(int));
-    printf("long..%d\n", (int)sizeof(long));
-    printf("long long..%d\n", (int)sizeof(long long));
-    printf("float..%d\n", (int)sizeof(float));
-    printf("double..%d\n", (int)sizeof(double));
+    int hoge = 5;
+    int piyo = 10;
+    int *hoge_p;
+
+    printf("&hoge: %p\n", (void*)&hoge);
+    printf("&piyo: %p\n", (void*)&piyo);
+    printf("&hoge_p: %p\n", (void*)&hoge_p);
+
+    hoge_p = &hoge;
+    printf("hoge_p: %p\n", (void*)hoge_p);
+    printf("hoge_p: %d\n", *hoge_p);
+
+    *hoge_p = 10;
+    printf("hoge: %d\n", hoge);
+
+    return 0;
 }
