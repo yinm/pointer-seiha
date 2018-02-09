2374cdd6e1515fc164c8a0ec68960705ebb47e50
yinm <yinm.cminor9@gmail.com>
2018-02-09 14:54:21 +0900

    Check array;

--- chap1/07.c
+++ chap1/07.c
@@ -10,9 +10,8 @@ int main(void)
         array[i] = i;
     }
 
-    for (p = &array[0]; p != &array[5]; p++) {
-        printf("%d\n", *p);
+    p = &array[0];
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", *(p + i));
     }
-
-    return 0;
 }
